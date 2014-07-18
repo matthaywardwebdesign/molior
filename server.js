22f06a3e0fc7d51cfc6caae5637a989c0729af47
Matthew HAYWARD <matt@matthayward.com.au>
2014-07-19 09:21:59 +1000

    Added xcodebuild warning and error parsing.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var path = require("path");
 var ensureDir = require("ensureDir");
 var crypto = require("crypto");
 var walk = require("walk");
-var downloading = [];
+var ip = require("ip");
 var exec = require("child_process").exec;
 
 //Create projects directory
@@ -20,7 +20,6 @@ fs.mkdir('projects/', function (err){});
 io.on('connection', function(socket){
     console.log("Client Connected");
     socket.on('fileSync', function(data){
-        downloading[data.project] = 0;
         //File sync requested
         fileSync(data);
     });
@@ -33,13 +32,15 @@ io.on('connection', function(socket){
     socket.on('build', function (data){
        buildProject(data, function (result){
             socket.emit('buildresult', {'result': result});   
-       }); 
+       },socket); 
     });
     socket.on('disconnect', function(){});
 });
 
 //Listen for requests
 server.listen(APP_PORT);
+
+console.log("App listening on " + ip.address() + ":" + APP_PORT);
 
 function fileSync(data){
     //Check if the file exists
@@ -147,7 +148,7 @@ function checkFiles(data){
     });
 }
 
-function buildProject(data, callback){
+function buildProject(data, callback, socket){
     var done = false;
     console.log("Building.....");
     var path = "projects/" + data.project + "/" + data.builddir; 
@@ -155,8 +156,8 @@ function buildProject(data, callback){
     path = path.split("\\").join("/");
     exec("cd " + path + ";xcodebuild > " + process.cwd() + "/projects/" + data.project + ".buildlog 2>&1", function (error, stdout, stderr){
         //Build complete - find result from log file
-        fs.readFile(process.cwd() + "/projects/" + data.project + ".buildlog", function (err, data) {
-            if (data.toString().indexOf("** BUILD SUCCEEDED **") > -1){
+        fs.readFile(process.cwd() + "/projects/" + data.project + ".buildlog", function (err, filedata) {
+            if (filedata.toString().indexOf("** BUILD SUCCEEDED **") > -1){
                 //Build passed
                 console.log("Build Passed");
                 if (done == false){
@@ -171,6 +172,128 @@ function buildProject(data, callback){
                  done = true;   
                 }
             }
+            
+            
+            
+            //Read each file line by line and find warning, notes and errors
+            var lines = filedata.toString().split("\n");
+            console.log("Total lines in build log: " + lines.length);
+            for (var i = 0; i < lines.length; i++){
+                var line = lines[i];
+                
+                //Check for compile line (clear warnings and errors for that file)
+                if (line.indexOf("CompileC") == 0){
+                       socket.emit("filecompile", {'file':line.split(" ")[2]});
+                }
+                //Find warnings
+                if (line.indexOf("warning:") > -1){
+                    var warningType = "";
+                    var warningData = {};
+                    if (line.split("warning:")[0] == "ld: "){
+                        warningType = "linker";   
+                    }
+                    if (line.indexOf("warning:") == 0){
+                        warningType = "plain";   
+                    }
+                    if (warningType == ""){
+                        warningType = "code";   
+                    }
+                    
+                    if (warningType == "linker"){
+                        warningData['type'] = "linker";
+                        warningData['info'] = line.split("warning: ")[1];
+                        //Due to the way the xcodebuild cache works, linker warnings cannot be supported at this stage
+                    }
+                    
+                    if (warningType == "plain"){
+                        warningData['type'] = "linker";
+                        warningData['info'] = line.split("warning: ")[1];
+                        //Due to the way the xcodebuild cache works, plain non code warning cannot be supported at this stage
+                    }
+                    
+                    if (warningType == "code"){
+                        warningData['type'] = "code";
+                        warningData['info'] = line.split("warning: ")[1].split("[").join(" ").split(":").join("-").split("]").join("").split("\'").join("");
+                        var warningFile = line.split("warning: ")[0].split(":")[0].split(data.project)[1];
+                        var warningInfo = line.split("warning: ")[1].split("[").join("*");
+                        socket.emit("filewarning", {"file": warningFile, "warning": warningData});
+                    }
+                    
+                }
+                
+                 //Find notes
+                if (line.indexOf("note:") > -1){
+                    var noteType = "";
+                    var noteData = {};
+                    if (line.split("note:")[0] == "ld: "){
+                        noteType = "linker";   
+                    }
+                    if (line.indexOf("note:") == 0){
+                        noteType = "plain";   
+                    }
+                    if (noteType == ""){
+                        noteType = "code";   
+                    }
+                    
+                    if (noteType == "linker"){
+                        noteData['type'] = "linker";
+                        noteData['info'] = line.split("note: ")[1];
+                        //Due to the way the xcodebuild cache works, linker notes cannot be supported at this stage
+                    }
+                    
+                    if (noteType == "plain"){
+                        noteData['type'] = "linker";
+                        noteData['info'] = line.split("note: ")[1];
+                        //Due to the way the xcodebuild cache works, plain non code notes cannot be supported at this stage
+                    }
+                    
+                    if (noteType == "code"){
+                        noteData['type'] = "code";
+                        noteData['info'] = line.split("note: ")[1].split("[").join(" ").split(":").join("-").split("]").join("").split("\'").join("");
+                        var noteFile = line.split("note: ")[0].split(":")[0].split(data.project)[1];
+                        var noteInfo = line.split("note: ")[1].split("[").join("*");
+                        socket.emit("filenote", {"file": noteFile, "note": noteData});
+                    }
+                    
+                }
+                
+                 //Find errors
+                if (line.indexOf("error:") > -1){
+                    var errorType = "";
+                    var errorData = {};
+                    if (line.split("error:")[0] == "ld: "){
+                        errorType = "linker";   
+                    }
+                    if (line.indexOf("error:") == 0){
+                        errorType = "plain";   
+                    }
+                    if (errorType == ""){
+                        errorType = "code";   
+                    }
+                    
+                    if (errorType == "linker"){
+                        errorData['type'] = "linker";
+                        errorData['info'] = line.split("error: ")[1];
+                        //Due to the way the xcodebuild cache works, linker errors cannot be supported at this stage
+                    }
+                    
+                    if (errorType == "plain"){
+                        errorData['type'] = "linker";
+                        errorData['info'] = line.split("error: ")[1];
+                        //Due to the way the xcodebuild cache works, plain non code errors cannot be supported at this stage
+                    }
+                    
+                    if (errorType == "code"){
+                        errorData['type'] = "code";
+                        errorData['info'] = line.split("error: ")[1].split("[").join(" ").split(":").join("-").split("]").join("").split("\'").join("");
+                        var errorFile = line.split("error: ")[0].split(":")[0].split(data.project)[1];
+                        var errorInfo = line.split("error: ")[1].split("[").join("*");
+                        socket.emit("fileerror", {"file": errorFile, "error": errorData});
+                    }
+                    
+                }
+            }
+        
         });
         
     });
